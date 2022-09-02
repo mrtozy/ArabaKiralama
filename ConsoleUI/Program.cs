42b942e921d1e96565bcf2c956f0ddfd1dcde93a
m-ozs <m-ozs@MRTOZY>
2022-09-02 18:32:02 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+          
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+            foreach (var car in carManager.GetCarsByColorId(2))
+            {
+                Console.WriteLine( car.ColorId + ". " + car.Description);
+            }
+            foreach (var car in carManager.GetCarsByBranddId(1))
+            {
+                Console.WriteLine(car.CarId + ". " + car.Description);
+            }
+        }
+    }
+}
